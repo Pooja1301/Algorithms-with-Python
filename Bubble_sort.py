ea36f1affba8730433466e2273297f1cb1839019
Pooja1301 <mailme.pooja04@gmail.com>
2017-11-18 00:48:02 +0530

    adding bubble sort

---
+++ Bubble_sort.py
@@ -0,0 +1,13 @@
+def bubble_sort(arr):
+    no_of_element = len(arr)
+    for k in range(0, no_of_element - 1):
+        for i in range(0, no_of_element - k - 1):
+            if arr[i] > arr[i+1]:
+                temp = arr[i]
+                arr[i] = arr[i + 1]
+                arr[i + 1] = temp
+
+
+arr = [7,4,5,2]
+bubble_sort(arr)
+print(arr)
