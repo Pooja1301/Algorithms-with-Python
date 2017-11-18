ba2623a5f2b71adfe7475c5dcd16f536fab5c344
Pooja1301 <mailme.pooja04@gmail.com>
2017-11-18 23:33:06 +0530

    code refactoring

--- Bubble_sort.py
+++ Bubble_sort.py
@@ -1,8 +1,10 @@
+def swap(first_num, another_num):
+    return another_num,first_num
+
+
 def bubble_sort(arr):
     no_of_element = len(arr)
     for k in range(0, no_of_element - 1):
         for i in range(0, no_of_element - k - 1):
             if arr[i] > arr[i+1]:
-                temp = arr[i]
-                arr[i] = arr[i + 1]
-                arr[i + 1] = temp
+                arr[i], arr[i + 1] = swap(arr[i],arr[i+1])
